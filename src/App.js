779779098c2cd568ab055f2697c147e7cd9168ab
Michael Harrington <michael.harrington@zoom.us>
2020-02-28 22:45:22 -0700

    req to signature

--- src/App.js
+++ src/App.js
@@ -2,23 +2,40 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  state = { loading: false, signature: '' }
+  state = { 
+    loading: false, 
+    signature: '',
+    // meetingLaunched: false,
+    // meetingNumber: "2859761343",
+    // leaveUrl: "#",
+    // userName: "",
+    // userEmail: "",
+    // passWord: "",
+    // role: 0,
+    meetingData: {
+      meetingNumber: "2859761343",
+      role: 0
+    }
+  }
 
-  handleClick = api => e => {
+  launchMeeting = meetingData => e => {
     e.preventDefault()
-
+    
     this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
+    fetch("/.netlify/functions/signature",{
+      method: "POST",
+      body: meetingData
+    })
       .then(res => res.json())
       .then(data => this.setState({ loading: false, signature: data.signature }))
   }
 
   render() {
-    const { loading, signature } = this.state
+    const { loading, signature, meetingData } = this.state
     return (
       <div className="App">
         <p>Signature: <br/>{signature}</p>
-        <button onClick={this.handleClick("signature")}>{loading ? "Loading..." : "Get Signature"}</button>
+        <button onClick={this.launchMeeting(meetingData)}>{loading ? "Loading..." : "Get Signature"}</button>
       </div>
     );
   }
--- src/lambda/signature.js
+++ src/lambda/signature.js
@@ -1,23 +1,36 @@
 const crypto = require('crypto')
+require('dotenv/config')
 
-function generateSignature(apiKey, apiSecret, meetingNumber, role) {
+function generateSignature(secrets, meetingData) {
     const timestamp = new Date().getTime()
-    const msg = Buffer.from(apiKey + meetingNumber + timestamp + role).toString('base64')
-    const hash = crypto.createHmac('sha256', apiSecret).update(msg).digest('base64')
-    const signature = Buffer.from(`${apiKey}.${meetingNumber}.${timestamp}.${role}.${hash}`).toString('base64')
-    
+    const msg = Buffer.from(secrets.apiKey + meetingData.meetingNumber + timestamp + meetingData.role).toString('base64')
+    const hash = crypto.createHmac('sha256', secrets.apiSecret).update(msg).digest('base64')
+    const signature = Buffer.from(`${secrets.apiKey}.${meetingData.meetingNumber}.${timestamp}.${meetingData.role}.${hash}`).toString('base64')
     return signature
 }
 
-export function handler(event, context) {
+const temp = {
+    body: {
+        meetingData: {
+            meetingNumber: '2859761343',
+            role: 0
+        }
+    }
+}
+
+export async function handler(event, context) {
     if (event.httpMethod !== "POST") {
         return { statusCode: 405, body: "Method Not Allowed" };
     }
 
-    try {
+    // console.log(event.body)
+    // console.log(context)
+
+    try {        
+
         return {
             statusCode: 200,
-            body: JSON.stringify({ signature: generateSignature(apiKey, apiSecret, meetingNumber, role) })
+            body: JSON.stringify({ signature: generateSignature(process.env, event.body) })
         }
     } catch (err) {
         console.log(err) // output to netlify function log
